f2d304d067d7f770c44f5987b18d39352598ddcf
wu5Dance <1018965709@qq.com>
2023-06-28 21:44:57 +0800

    更改publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 关闭eslint校验
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: './',
 })
